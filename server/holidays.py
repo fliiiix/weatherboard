cb831698240add438e11814224e86bd92bd4db32
fliiiix <hi@l33t.name>
2021-09-04 11:24:02 +0200

    udpate dates

--- server/holidays.py
+++ server/holidays.py
@@ -1,24 +1,11 @@
 import datetime
 
+# idk some dates from https://publicholidays.ch/de/2021-dates/
+
 holidays = {
-    datetime.date(2021, 9, 6): "Labor Day",
-    datetime.date(2021, 10, 11): "Columbus Day",
-    datetime.date(2021, 11, 11): "Veterans Day",
-    datetime.date(2021, 11, 25): "Thanksgiving",
-    datetime.date(2021, 12, 25): "Christmas",
-    datetime.date(2022, 1, 1): "New Year",
-    datetime.date(2022, 1, 17): "MLK Day",
-    datetime.date(2022, 2, 21): "Presidents' Day",
-    datetime.date(2022, 3, 27): "Mothers' Day UK",
-    datetime.date(2022, 5, 9): "Mothers' Day US",
-    datetime.date(2022, 5, 30): "Memorial Day",
-    datetime.date(2022, 6, 19): "Fathers' Day",
-    datetime.date(2022, 6, 20): "Juneteenth",
-    datetime.date(2022, 7, 4): "Independence Day",
-    datetime.date(2022, 9, 5): "Labor Day",
-    datetime.date(2022, 10, 10): "Columbus Day",
-    datetime.date(2022, 11, 11): "Veterans Day",
-    datetime.date(2022, 11, 24): "Thanksgiving",
-    datetime.date(2022, 12, 25): "Christmas",
-    datetime.date(2023, 1, 1): "New Year",
+    datetime.date(2021, 11, 1): "Allerheiligen",
+    datetime.date(2021, 12, 8): "Mariä Empfängnis",
+    datetime.date(2021, 12, 25): "Weihnachtstag",
+    datetime.date(2021, 12, 26): "Stephanstag",
+    datetime.date(2021, 12, 31): "Silvester",
 }
